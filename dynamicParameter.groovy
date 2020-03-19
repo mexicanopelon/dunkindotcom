90c2a5207a35cdb397c4e642dd610de8dee2b294
mexicanopelon <mexicanopelon@hotmail.com>
2020-03-19 09:34:49 -0500

    Fix 2

--- dynamicParameter.groovy
+++ dynamicParameter.groovy
@@ -11,6 +11,14 @@ def ARTIFACTORY_REPO = "ddcom-release-prod"
 
 ["git", "clone", "git@github.com:mexicanopelon/dunkindotcom.git"].execute()
 
+def inputFile = new File("./dunkindotcom/dev-properties-1.json")
+def data = new JsonSlurper().parseFile(inputFile, 'UTF-8')
+
+data.servers_list.each{ 
+    def serverGroup = it.keySet()
+    print(serverGroup)
+}
+
 pipeline {
     agent any
 
@@ -24,7 +32,7 @@ pipeline {
 
     parameters { 
         string(name: 'CODE_BRANCH', defaultValue: '', description: 'Branch Name')
-        choice(name: 'PUBLISHER', choices: ['PUB1', 'PUB2', 'PUB3', 'PUB4'], description: 'Deploy On')
+        choice(name: 'PUBLISHER', choices: ${serverGroup}, description: 'Deploy On')
         string(defaultValue: "https://dunkindonuts.com", description: 'URL Check', name: 'URL')
         string(defaultValue: "content/dd/en.html", description: 'URL Value', name: 'URL_VAL')
     }
