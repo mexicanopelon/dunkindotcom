1b2342cf4d3285cdd8a4f879df42833fd0ef3263
mexicanopelon <mexicanopelon@hotmail.com>
2020-03-23 15:05:20 -0500

    Retrying 5

--- dynamicParameterEx3.groovy
+++ dynamicParameterEx3.groovy
@@ -63,6 +63,7 @@ pipeline {
             steps {
                 script{
                     sh "echo HELLO WORLD!!!"
+                    sh "echo WORKSPACE is ${env.WORKSPACE}"
                 }
             }
         }   
