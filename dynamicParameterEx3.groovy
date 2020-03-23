6b1d4b10c0c695b063534e9794e8d01f7c9f519a
mexicanopelon <mexicanopelon@hotmail.com>
2020-03-23 16:13:56 -0500

    Retrying 17

--- dynamicParameterEx3.groovy
+++ dynamicParameterEx3.groovy
@@ -32,7 +32,7 @@ properties([
                     //def workspace = this.binding.jenkinsProject.workspace
                     def workspace = "/Users/cdelapaz/.jenkins/workspace/try_again3"
 
-                    return ["${workspace}"]
+                    return ["${workspace}","test"]
                     def inputFile = new File("${workspace}/dunkindotcom/dev-properties.json")
                     def data = new JsonSlurper().parseFile(inputFile, 'UTF-8')
 
