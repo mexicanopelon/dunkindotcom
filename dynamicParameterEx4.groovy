09cf5a9a5b6a4bdd6a1d5cc2e5e1fd76a2ee6e8e
mexicanopelon <mexicanopelon@hotmail.com>
2020-03-23 15:13:55 -0500

    Retrying 6

---
+++ dynamicParameterEx4.groovy
@@ -0,0 +1,71 @@
+#!/usr/bin/env groovy
+
+def DEPLOYMENT_SCRIPT_BRANCH
+def ARTIFACT_NAME
+def ARTIFACT_VERSION
+def ART_NAME = 'ddcom-ui'
+def PUBLISHER_PORT = "4503"
+def ARTIFACTORY_REPO = "ddcom-release-prod"
+def serverGroup
+
+properties([
+    parameters([
+        [
+            $class: 'ExtendedChoiceParameter', 
+            choiceType: 'PT_SINGLE_SELECT',
+            description: '', 
+            filterable: false, 
+            name: 'PUBLISHER', 
+            randomName: 'choice-parameter-21337077649621572', 
+            script: [
+                $class: 'GroovyScript', 
+                fallbackScript: [
+                    classpath: [], sandbox: false, script: ''
+                ], 
+                script: [
+                    classpath: [], sandbox: false, script: 
+                    '''
+                    import groovy.json.JsonSlurper
+
+                    ["git", "clone", "git@github.com:mexicanopelon/dunkindotcom.git"].execute()
+   
+                    def workspace = this.binding.jenkinsProject.workspace
+
+                    return ["${workspace}"]
+                    def inputFile = new File("${workspace}/dunkindotcom/dev-properties.json")
+                    def data = new JsonSlurper().parseFile(inputFile, 'UTF-8')
+
+                    data.servers_list.each{ 
+                        serverGroup =  it.keySet()
+                    }
+
+                    return serverGroup as List
+                    '''
+                ]
+            ]
+        ]
+    ])
+])
+
+pipeline {
+    agent any
+
+    options {
+        buildDiscarder(logRotator(daysToKeepStr: '', numToKeepStr: '5'))
+    }
+
+    environment {
+        ARTIFACTORY_SERVER = 'Dunkin_artifactory'
+    }
+
+    stages {
+        stage('BUILD') {
+            steps {
+                script{
+                    sh "echo HELLO WORLD!!!"
+                    sh "echo WORKSPACE is ${env.WORKSPACE}"
+                }
+            }
+        }   
+    }
+}
