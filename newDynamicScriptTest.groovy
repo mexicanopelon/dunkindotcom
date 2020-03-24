55a715ec0d8c148494d6568af52bf52211b1d844
mexicanopelon <mexicanopelon@hotmail.com>
2020-03-24 12:46:58 -0500

    New script test 2

--- newDynamicScriptTest.groovy
+++ newDynamicScriptTest.groovy
@@ -10,3 +10,25 @@ properties([
         choice(name: 'PARAM', choices: JustTest().join('\n'), description: 'Choice'),
     ])
 ])
+
+pipeline {
+    agent any
+
+    options {
+        buildDiscarder(logRotator(daysToKeepStr: '', numToKeepStr: '5'))
+    }
+
+    environment {
+        ARTIFACTORY_SERVER = 'Dunkin_artifactory'
+    }
+
+    stages {
+        stage('BUILD') {
+            steps {
+                script{
+                    sh "echo HELLO WORLD!!!"
+                }
+            }
+        }   
+    }
+}
