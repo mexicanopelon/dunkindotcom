ca19de475369e0893dfcf06b2e1011961725bcc8
mexicanopelon <mexicanopelon@hotmail.com>
2020-03-24 13:00:12 -0500

    New script test 4

---
+++ multiSelectTest.groovy
@@ -0,0 +1,40 @@
+import com.cwctravel.hudson.plugins.extended_choice_parameter.ExtendedChoiceParameterDefinition
+
+node {
+    def multiSelect= new ExtendedChoiceParameterDefinition("name", 
+                "PT_MULTI_SELECT", 
+                "blue,green,yellow,blue", 
+                "project name",
+                "", 
+                "",
+                "", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "blue,green,yellow,blue", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "", 
+                "", 
+                false,
+                false, 
+                3, 
+                "multiselect", 
+                ",") 
+
+    def userInput = input  id: 'customID', message: 'Let\'s promote?', ok: 'Release!', parameters:  [multiSelect]
+
+
+    echo "Hello: "+ userInput
+}
