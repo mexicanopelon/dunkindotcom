c3332080bcc63838d928eb006867b480e484feec
mexicanopelon <mexicanopelon@hotmail.com>
2020-03-25 15:57:53 -0500

    Adding new list script 15

--- newDynamicScriptTestMultiSelect.groovy
+++ newDynamicScriptTestMultiSelect.groovy
@@ -35,34 +35,6 @@ List GetParamList() {
     return options as List
 }
 
-List getEnvMap() {
-    ["rm", "-Rf", "/tmp/dunkindotcom"].execute()
-    ["git", "clone", "git@github.com:mexicanopelon/dunkindotcom.git", "/tmp/dunkindotcom"].execute()
-    sleep(5)
-
-    def inputFile = new File("/tmp/dunkindotcom/tagsProperties.json")
-    def data = new JsonSlurper().parseFile(inputFile, 'UTF-8')
-    def envMap = [:]
-    data.Environment.each{
-        it.keySet().each{
-            env = it.toString()
-            serverGroup = data.Environment.getAt("$it").ServerGroup
-            serverGroup.each{
-                it.each{
-                    it.each{
-                        it.keySet().each {
-                            envMap.put("'${it}'","'${env}'")
-                            
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    return envMap
-}
-
 properties([
     parameters([
         choice(name: 'PARAM', choices: GetParamList().join('\n'), description: 'Choice'),
@@ -77,6 +49,36 @@ pipeline {
             steps {
                 script{
                     sh "echo HELLO WORLD!!!"
+
+                    List getEnvMap() {
+                        ["rm", "-Rf", "/tmp/dunkindotcom"].execute()
+                        ["git", "clone", "git@github.com:mexicanopelon/dunkindotcom.git", "/tmp/dunkindotcom"].execute()
+                        sleep(5)
+
+                        def inputFile = new File("/tmp/dunkindotcom/tagsProperties.json")
+                        def data = new JsonSlurper().parseFile(inputFile, 'UTF-8')
+                        def envMap = [:]
+                        data.Environment.each{
+                            it.keySet().each{
+                                env = it.toString()
+                                serverGroup = data.Environment.getAt("$it").ServerGroup
+                                serverGroup.each{
+                                    it.each{
+                                        it.each{
+                                            it.keySet().each {
+                                                envMap.put("'${it}'","'${env}'")
+                                                
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        return envMap
+                    }
+
+                    println getEnvMap()
                 }
             }
         }   
