62a49f30af7417239ac46f62246c02dd9a61f111
mexicanopelon <mexicanopelon@hotmail.com>
2020-03-26 13:46:29 -0500

    Adding another script test 13

--- newDynamicScriptTestMultiSelect2.groovy
+++ newDynamicScriptTestMultiSelect2.groovy
@@ -31,7 +31,7 @@ def GetParamList() {
     ["rm", "-Rf", "/tmp/dunkindotcom"].execute()
     sleep(1000)
     ["git", "clone", "git@github.com:mexicanopelon/dunkindotcom.git", "/tmp/dunkindotcom"].execute()
-    sleep(4000)
+    sleep(5000)
 
     def inputFile = new File("/tmp/dunkindotcom/tagsProperties.json")
     def data = new JsonSlurper().parseFile(inputFile, 'UTF-8')
