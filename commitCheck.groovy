916475a9f72460eb6b229d46a95d6397aafcdede
mexicanopelon <mexicanopelon@hotmail.com>
2020-04-01 12:01:54 -0500

    Adding a commit check script.

---
+++ commitCheck.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+import groovy.json.JsonSlurper
+
+
+["rm", "-Rf", "/tmp/dunkindotcom"].execute()
+["git", "clone", "git@github.com:mexicanopelon/dunkindotcom.git", "/tmp/dunkindotcom"].execute()
+
+def commitChanges = ['git, 'show', '--pretty=""', '--name-only'].execute().text
+println commitChanges
+
+return commitChanges.contains("commitCheck.groovy")
+
+
