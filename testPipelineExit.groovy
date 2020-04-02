0c10152f47b2096836768e55784ba0c3df457d75
mexicanopelon <mexicanopelon@hotmail.com>
2020-04-02 12:24:32 -0500

    Added code to test pipeline exit 8

--- testPipelineExit.groovy
+++ testPipelineExit.groovy
@@ -11,21 +11,7 @@ def getBuildUser() {
         return GET_BUILD_USER
     }
 }
-def GetPub(){
-    node{
-        git branch: "master",
-        url: "git@github.com:mexicanopelon/dunkindotcom.git"
-        def props = readJSON file: 'tagsProperties.json', returnPojo: true
-        def options = []
-        def count = '1';
-        props.Environment.get(0)."${params.Environment}".get(0).ServerGroup.get(0).each{ key, value ->
-            echo "PUB$count"
-            options.add("PUB$count")
-        count++
-        }
-        return options as List
-    }
-}
+
 pipeline {
     agent any
     options {
@@ -33,7 +19,7 @@ pipeline {
     }
     parameters {
         choice(name: 'Environment', choices: ['Dev', 'Qa', 'Stag', 'Prod'], description: 'Deploy On')
-        choice(name: 'Publisher', choices: GetPub(), description: 'Publisher Deploy On')
+        choice(name: 'Publisher', choices: ['Pub1', 'PUb2'], description: 'Publisher Deploy On')
         choice(name: 'UpdateBuild', choices: ['False', 'True'], description: 'Update the job')
     }
     environment {
