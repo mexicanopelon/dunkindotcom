c3e55395006ac568bb18d44459a9fcf494da4c2d
mexicanopelon <mexicanopelon@hotmail.com>
2020-04-03 09:41:31 -0500

    Add getEnvTest.groovy

---
+++ getEnvTest.groovy
@@ -0,0 +1,66 @@
+#!/usr/bin/groovy
+def SLACK_CHANNEL
+def COLOUR_MAP = ['SUCCESS': 'good', 'FAILURE': 'danger', 'UNSTABLE': 'danger', 'ABORTED': 'danger']
+def getBuildUser() {
+    node {
+        wrap([$class: 'BuildUser']) {
+            GET_BUILD_USER = sh ( script: 'echo "${BUILD_USER}"', returnStdout: true).trim()
+        }
+        return GET_BUILD_USER
+    }
+}
+def GetPub(){
+    node{
+        git branch: "common",
+        url: "git@github.com:mexicanopelon/dunkindotcom.git"
+        def props = readJSON file: 'tagsProperties.json', returnPojo: true
+        def options = []
+        def count = '1';
+        props.Environment.get(0).Dev.get(0).ServerGroup.get(0).each{ key, value ->
+            echo "PUB$count"
+            options.add("PUB$count")
+        count++
+        }
+        return options as List
+    }
+}
+
+def GetEnv(){
+    node{
+        git branch: "common",
+        url: "git@github.com:mexicanopelon/dunkindotcom.git"
+        def props = readJSON file: 'tagsProperties.json', returnPojo: true
+        def options = []
+        def count = '1';
+        return props.Environment.get(0).each.get(0)
+    }
+}
+
+pipeline {
+    agent any
+    options {
+        buildDiscarder(logRotator(daysToKeepStr: '', numToKeepStr: '5'))
+    }
+    parameters {
+        choice(name: 'Environment', choices: GetEnv(), description: 'Deploy On')
+        choice(name: 'Publisher', choices: "Pub1, Pub2", description: 'Publisher Deploy On')
+        choice(name: 'UpdateBuild', choices: ['False', 'True'], description: 'Update the job')
+    }
+    environment {
+        BUILD_USER = ''
+    }
+    stages {
+        stage('UPDATE'){
+            when {
+                expression {
+                    params.UpdateBuild == 'True'
+                }
+            }
+            steps{
+                script{
+                    echo "Build Updated"
+                }
+            }
+        }
+    }
+}
